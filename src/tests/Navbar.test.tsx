2c98d1f1634833a9c312f015575bb2e68963255f
Sujeewa Sandeepa Kularathna <sujeewasandeepa@proton.me>
2023-06-04 19:00:57 +0530

    install jest

---
+++ src/tests/Navbar.test.tsx
@@ -0,0 +1,7 @@
+// import NavBar from '../components/NavBar.tsx';
+// import { render, screen } from '@testing-library/react';
+//
+// test("renders navbar", () => {
+//   render(<NavBar/>);
+//   const title = screen.getByDisplayValue("Traditional Knowledge");
+// });
