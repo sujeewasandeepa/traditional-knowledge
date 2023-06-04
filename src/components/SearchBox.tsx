180a5213150554ca59c10662f3e48c2ef4833ee6
Sujeewa Sandeepa Kularathna <sujeewasandeepa@proton.me>
2023-06-04 19:01:22 +0530

    style search bar and put an image

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,8 +1,15 @@
+import "../assets/SearchBar.css";
+
 export default function SearchBox () {
   return (
   <>
-      <div>
-        sassy search box UwU
+      <div className="has-text-centered">
+        <input 
+          type="text"
+          className="input mt-5"
+          id="searchBox"
+          placeholder="Search"
+        />
       </div>
   </>
   )
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,21 @@
-import NavBar from "../components/NavBar"
-import SearchBox from "../components/SearchBox"
+import NavBar from "../components/NavBar";
+import SearchBox from "../components/SearchBox";
+import homeImage from "../assets/home-image.jpg";
+import "../assets/home-image.css";
 
 export default function Home () {
   return (
   <>
       <NavBar/>
+
+      <div className="columns is-centered">
+        <div className="column is-half">
+          <figure className="image is-centered">
+            <img src={homeImage} className="is-centered home-image"/>
+          </figure>
+        </div>
+      </div>
+
       <SearchBox/>
   </>
   )
