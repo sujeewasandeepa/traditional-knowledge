f5d46b9476e8f9a16d3d37273a16961683aa8019
Sujeewa Sandeepa Kularathna <sujeewasandeepa@proton.me>
2023-06-06 22:40:25 +0530

    create login page

--- src/routes/LogIn.tsx
+++ src/routes/LogIn.tsx
@@ -1,7 +1,43 @@
+import "../assets/Login.css"
+
 export default function LogIn () {
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('submit clicked!');
+    }
+
+    const handleCancel = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        console.log('cancel clicked!');
+    }
+
   return (
   <>
-    <h1>LogIn</h1>
+    <div className="has-text-centered">
+        <h1 className="m-5 title is-2">LogIn</h1>
+
+        <form className="form" onSubmit={handleSubmit}>
+            <div className="field">
+                <label className="label">username</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="username" />
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label">password</label>
+                <div className="control">
+                    <input className="input" type="password" placeholder="password" />
+                </div>
+            </div>
+
+            <input type="submit" className="button is-primary" value="login" />
+            <button onClick={handleCancel} className="button ml-2">Cancel</button>
+
+        </form>
+
+    </div>
   </>
   )
 }
